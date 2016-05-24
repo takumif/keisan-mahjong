b2e56d40ea179c848c9d98dae5941568057738ab
Takumi Fujimoto <takumi@takumium.com>
2016-05-24 10:26:12 -0700

    Add jasmine testing to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
+/// <reference path="typings/index.d.ts" />
+
 var gulp = require("gulp");
 var ts = require("gulp-typescript");
 var tsProject = ts.createProject("tsconfig.json");
 var typedoc = require("gulp-typedoc");
+var jasmine = require("gulp-jasmine");
 
-gulp.task("default", function () {
+gulp.task("tsc", function () {
     return tsProject.src()
         .pipe(ts(tsProject))
         .js.pipe(gulp.dest("dist"));
@@ -16,7 +19,21 @@ gulp.task("typedoc", function() {
             module: "commonjs",
             target: "es5",
             out: "docs/",
-            name: "My project title"
+            name: "Riichi Mahjong"
         }))
     ;
 });
+
+gulp.task("test", function() {
+    return gulp
+        .src([
+            "test/test-tile.js"
+        ])
+        .pipe(jasmine())
+});
+
+gulp.task("watch", function() {
+    gulp.watch("src/**/*.ts", ["tsc", "typedoc"]);
+})
+
+gulp.task("default", ["tsc", "typedoc", "test"]);

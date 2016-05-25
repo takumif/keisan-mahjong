9f97a7cc7ee253080fdaa2fd48ebc7e86a6f1862
Takumi Fujimoto <takumi@takumium.com>
2016-05-24 19:21:07 -0700

    Move meld-related static methods from Tile to Meld

--- src/meld.ts
+++ src/meld.ts
@@ -12,6 +12,87 @@ export abstract class Meld {
         });
         return pairs === 1 || pairs === 7;
     }
+
+    /**
+     * Returns a list of possible ways to form melds with the given tiles.
+     * We assume that the tiles are closed, and hence there are no quadruples,
+     * and there should be one to five melds (one of which a pair), or seven pairs.
+     * An empty list is returned if all combinations end up with leftover tiles.
+     */
+    static formMelds(tiles: Tile[]): Meld[][] {
+        if (tiles.length % 3 !== 2 || tiles.length > 14) {
+            throw "Invalid number of tiles!";
+        }
+        var tiles = Tile.copyArray(tiles);
+        tiles.sort(Tile.compare);
+        
+        var meldLists = Meld.formMeldsWithSortedTiles(tiles);
+        
+        return meldLists.filter((melds, i, a) => {
+            return Meld.hasOneOrSevenPairs(melds);
+        });
+    }
+
+    /**
+     * Requires that the tiles are sorted.
+     */
+    private static formMeldsWithSortedTiles(tiles: Tile[]): Meld[][] {
+        if (tiles.length === 0) {
+            return [[]];
+        }
+        
+        var meldLists: Meld[][] = [];
+        
+        meldLists = meldLists
+            .concat(Meld.formMeldsWithFirstTileInPair(tiles))
+            .concat(Meld.formMeldsWithFirstTileInTriple(tiles))
+            .concat(Meld.formMeldsWithFirstTileInStraight(tiles));
+        
+        return meldLists;
+    }
+
+    /**
+     * Requires that the tiles are sorted
+     */
+    private static formMeldsWithFirstTileInPair(tiles: Tile[]): Meld[][] {
+        if (tiles.length < 2 || !tiles[0].equals(tiles[1])) {
+            return [];
+        }
+        var pair = new Pair(tiles[0], tiles[1]);
+        return Meld.formMeldsWithSortedTiles(tiles.slice(2)).map((melds, i, a) => {
+            return [pair].concat(melds);
+        });
+    }
+
+    /**
+     * Requires that the tiles are sorted
+     */
+    private static formMeldsWithFirstTileInTriple(tiles: Tile[]): Meld[][] {
+        if (tiles.length < 3 || !tiles[0].equals(tiles[1]) || !tiles[0].equals(tiles[2])) {
+            return [];
+        }
+        var triple = new Triple(tiles[0], tiles[1], tiles[2]);
+        return Meld.formMeldsWithSortedTiles(tiles.slice(3)).map((melds, i, a) => {
+            return [triple].concat(melds);
+        });
+    }
+
+    /**
+     * Requires that the tiles are sorted
+     */
+    private static formMeldsWithFirstTileInStraight(tiles: Tile[]): Meld[][] {
+        if (tiles.length < 3) {
+            return [];
+        }
+        var straight = Tile.extractStraight(tiles, tiles[0]);
+        if (straight === null) {
+            return [];
+        } else {
+            return Meld.formMeldsWithSortedTiles(straight.otherTiles).map((melds, i, a) => {
+                return [straight.straight].concat(melds);
+            });
+        }
+    }
 }
 
 export class Pair extends Meld {
--- src/tile.ts
+++ src/tile.ts
@@ -171,92 +171,11 @@ export class Tile {
         }
         return pairs;
     }
-
-    /**
-     * Returns a list of possible ways to form melds with the given tiles.
-     * We assume that the tiles are closed, and hence there are no quadruples,
-     * and there should be one to five melds (one of which a pair), or seven pairs.
-     * An empty list is returned if all combinations end up with leftover tiles.
-     */
-    static formMelds(tiles: Tile[]): Meld[][] {
-        if (tiles.length % 3 !== 2 || tiles.length > 14) {
-            throw "Invalid number of tiles!";
-        }
-        var tiles = Tile.copyArray(tiles);
-        tiles.sort(Tile.compare);
-        
-        var meldLists = Tile.formMeldsWithSortedTiles(tiles);
-        
-        return meldLists.filter((melds, i, a) => {
-            return Meld.hasOneOrSevenPairs(melds);
-        });
-    }
-
-    /**
-     * Requires that the tiles are sorted.
-     */
-    private static formMeldsWithSortedTiles(tiles: Tile[]): Meld[][] {
-        if (tiles.length === 0) {
-            return [[]];
-        }
-        
-        var meldLists: Meld[][] = [];
-        
-        meldLists = meldLists
-            .concat(Tile.formMeldsWithFirstTileInPair(tiles))
-            .concat(Tile.formMeldsWithFirstTileInTriple(tiles))
-            .concat(Tile.formMeldsWithFirstTileInStraight(tiles));
-        
-        return meldLists;
-    }
-
-    /**
-     * Requires that the tiles are sorted
-     */
-    private static formMeldsWithFirstTileInPair(tiles: Tile[]): Meld[][] {
-        if (tiles.length < 2 || !tiles[0].equals(tiles[1])) {
-            return [];
-        }
-        var pair = new Pair(tiles[0], tiles[1]);
-        return Tile.formMeldsWithSortedTiles(tiles.slice(2)).map((melds, i, a) => {
-            return [pair].concat(melds);
-        });
-    }
-
-    /**
-     * Requires that the tiles are sorted
-     */
-    private static formMeldsWithFirstTileInTriple(tiles: Tile[]): Meld[][] {
-        if (tiles.length < 3 || !tiles[0].equals(tiles[1]) || !tiles[0].equals(tiles[2])) {
-            return [];
-        }
-        var triple = new Triple(tiles[0], tiles[1], tiles[2]);
-        return Tile.formMeldsWithSortedTiles(tiles.slice(3)).map((melds, i, a) => {
-            return [triple].concat(melds);
-        });
-    }
-
-    /**
-     * Requires that the tiles are sorted
-     */
-    private static formMeldsWithFirstTileInStraight(tiles: Tile[]): Meld[][] {
-        if (tiles.length < 3) {
-            return [];
-        }
-        var straight = Tile.extractStraight(tiles, tiles[0]);
-        if (straight === null) {
-            return [];
-        } else {
-            return Tile.formMeldsWithSortedTiles(straight.otherTiles).map((melds, i, a) => {
-                return [straight.straight].concat(melds);
-            });
-        }
-    }
     
     /**
      * Returns null if not possible
      */
-    private static extractStraight(tiles: Tile[], firstTile: Tile): { straight: Straight; otherTiles: Tile[] } {
+    static extractStraight(tiles: Tile[], firstTile: Tile): { straight: Straight; otherTiles: Tile[] } {
         if (firstTile.type !== TileType.Number) {
             return null;
         }
